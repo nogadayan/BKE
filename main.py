30f8bd80138c2f91f5ac28d53b145a391d5ca01e
Noga Dayan <57255504+nogadayan@users.noreply.github.com>
2022-03-06 13:25:43 +0000

    Basis uit fundament "een eenvoudige tegenspeler" toegepast (=werkt niet)

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from _ml import MLAgent, train, save, load, train_and_plot
 from _core import is_winner, opponent, start
 
-start()
+
+class MyRandomAgent(EvaluationAgent):
+    def evaluate(self, board, my_symbol, opponent_symbol):
+        return random.randint(1,500)
+
+
+my_random_agent = MyRandomAgent()
+start(player_o=my_random_agent) 
