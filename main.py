a9ee8056afe762fc88d28952fa6b7dfc2c94442b
Noga Dayan <57255504+nogadayan@users.noreply.github.com>
2022-03-06 21:35:55 +0000

    keuze nr.4 (grafiek plotten) toegevoegd en "random_agent" weer toegevoegd (keuze nr. 4 werkte anders niet)

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ class MyAgent(MLAgent):
           reward = 0
       return reward
     
-
 random.seed(1)
 
 my_agent = MyAgent()
+random_agent = RandomAgent()
 
 print("Kies uit 1 van de volgende opties: \n 1. train een agent \n 2. speel tegen een getrainde agent \n 3. speel tegen een ander persoon \n 4. plot een validatie grafiek")
 
@@ -34,3 +34,10 @@ if choice == '2':
   start(player_x=my_agent)
 if choice == '3':
   start()
+if choice == '4':
+  train_and_plot(
+    agent=my_agent,
+    validation_agent=random_agent,
+    iterations=50,
+    trainings=100,
+    validations=1000)
