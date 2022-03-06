3dccdcdeca61fc99f917adbf4129e40d9aa8026b
Noga Dayan <57255504+nogadayan@users.noreply.github.com>
2022-03-06 15:12:50 +0000

    hyperparameters aangepast

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ random_agent = RandomAgent()
 train_and_plot(
     agent=my_agent,
     validation_agent=random_agent,
-    iterations=50,
+    iterations=60,
     trainings=100,
     validations=1000)
 
-my_agent = MyAgent(alpha=0.8, epsilon=0.2)
+my_agent = MyAgent(alpha=0.3, epsilon=0.7)
