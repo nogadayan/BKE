c5cd678345200f0d1de091840a73efd82e0f92d4
Noga Dayan <57255504+nogadayan@users.noreply.github.com>
2022-03-06 13:31:08 +0000

    vorige code verwijderd, nieuwe "train"-functie toegevoegd uit fundament theorie

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ from _ml import MLAgent, train, save, load, train_and_plot
 from _core import is_winner, opponent, start
 
 
-class MyRandomAgent(EvaluationAgent):
-    def evaluate(self, board, my_symbol, opponent_symbol):
-        return random.randint(1,500)
+class MyAgent(MLAgent):
+    def evaluate(self, board):
+        if is_winner(board, self.symbol):
+            reward = 1
+        elif is_winner(board, opponent[self.symbol]):
+            reward = -1
+        else:
+            reward = 0
+        return reward
 
+my_agent = MyAgent()
 
-my_random_agent = MyRandomAgent()
-start(player_o=my_random_agent) 
+train(my_agent, 3000)
+
+save(my_agent, 'MyAgent_3000')
