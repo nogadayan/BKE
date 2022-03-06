38d0e90bea9833b20bb802b2843f01194f4e1b26
Noga Dayan <57255504+nogadayan@users.noreply.github.com>
2022-03-06 13:49:28 +0000

    random import ingezet, "RandomAgent" nu bij goede import gezet en de getrainde agent "ingeladen"

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-from _ml import MLAgent, train, save, load, train_and_plot
+import random
+
+from _ml import MLAgent, train, save, RandomAgent, load, train_and_plot
 from _core import is_winner, opponent, start
 
-
 class MyAgent(MLAgent):
-    def evaluate(self, board):
-        if is_winner(board, self.symbol):
-            reward = 1
-        elif is_winner(board, opponent[self.symbol]):
-            reward = -1
-        else:
-            reward = 0
-        return reward
-
+  def evaluate(self, board):
+      if is_winner(board, self.symbol):
+           reward = 1
+      elif is_winner(board, opponent[self.symbol]):
+          reward = -1
+      else:
+          reward = 0
+      return reward
+    
+ 
 my_agent = MyAgent()
-
-train(my_agent, 3000)
-
-save(my_agent, 'MyAgent_3000')
+my_agent = load('MyAgent_3000')
+ 
+my_agent.learning = False
+ 
+start(player_x=my_agent)
